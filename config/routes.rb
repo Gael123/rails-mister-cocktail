e47d825e14d582f0fdab51172a860750472d2e17
Gael Jerop <gael.jerop@yahoo.com>
2019-08-23 14:40:14 +1000

    created all the views can show all cocktails and create a new one and link to doses

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   resources :cocktails ,only: [ :new,:show,:create,:index] do
-    resources :doses,only: [ :create,:new,]
+    resources :doses,only: [ :new, :create]
 end
-resources :doses,only:[:destroy]
+resources :doses, only:[:destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
