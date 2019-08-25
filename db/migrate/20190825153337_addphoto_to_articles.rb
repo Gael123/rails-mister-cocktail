b1ad3cf1aadc2b825e7aec6f926358b78cebb3e6
Gael Jerop <gael.jerop@yahoo.com>
2019-08-26 02:29:40 +1000

    final changes on mr cocktail

---
+++ db/migrate/20190825153337_addphoto_to_articles.rb
@@ -0,0 +1,4 @@
+class AddphotoToArticles < ActiveRecord::Migration[5.2]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_22_021959) do
+ActiveRecord::Schema.define(version: 2019_08_25_153337) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
